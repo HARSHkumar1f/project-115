e97f690a4dcf1d44a146c6a7f84cee56abf70298
HARSHkumar1f <98209420+HARSHkumar1f@users.noreply.github.com>
2023-04-02 15:19:07 +0530

    Add files via upload

---
+++ c115.py
@@ -0,0 +1,8 @@
+import os
+
+source= "/Users/apple/Desktop/shalter.jpg"
+
+dest = "/Users/apple/Desktop/mp.jpg"
+
+os.rename(source,dest)
+print("Source path renamed to destination path succesfully.")
